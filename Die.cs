d19f47f2803729ac37bd55c38ce880ea69a5b3c5
melyman95 <mattlyman12@gmail.com>
2021-07-19 15:24:15 -0700

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Represents an individual dice.
+    /// </summary>
+    class Die
+    {
+        /// <summary>
+        /// face value of die.
+        /// </summary>
+        public byte FaceValue { get; private set; }
+
+        public bool IsHeld { get; set; }
+
+        public byte RollDie()
+        {
+            Random r = new Random();
+
+            byte DieValue = (byte)r.Next(1, 6);
+
+            return DieValue;
+        }
+    }
+}
