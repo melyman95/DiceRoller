ecdc2e460e45650a054b6048a9724101497b9d6b
melyman95 <mattlyman12@gmail.com>
2021-07-19 21:42:16 -0700

    added RollDie to IsHeld

--- Die.cs
+++ Die.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace DiceRoller
 {
     /// <summary>
-    /// Represents an individual dice.
+    /// Represents a six-sided die like one used in Monopoly games.
     /// </summary>
     class Die
     {
@@ -24,24 +24,27 @@ namespace DiceRoller
         }
         public Die()
         {
-            if (r == null)
-            {
-                r = new Random();
-            }
-            else
-            {
                 // Roll die on creation to generate first random number.
                 RollDie();
-            }
         }
         public byte FaceValue { get; private set; }
 
         public bool IsHeld { get; set; }
 
+        /// <summary>
+        /// Rolls random value between 1 and 6.
+        /// Return the newly rolled value.
+        /// If the die is held, the current value will be returned.
+        /// </summary>
+        /// <returns></returns>
+
         public byte RollDie()
         {
-            int DieValue = r.Next(1, 7);
-            FaceValue = (byte)DieValue;
+            if (!IsHeld) // Die is not held.
+            {
+                int DieValue = r.Next(1, 7);
+                FaceValue = (byte)DieValue;
+            }
 
             return FaceValue;
         }
